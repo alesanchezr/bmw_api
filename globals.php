2aa47c5ce43f18dcd0fbbfa88bf9a7c5801d6065
Alejandro Sanchez <aalejo@gmail.com>
2018-06-12 14:53:54 +0000

    initial comit

--- globals.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('API_DEBUG', false);
-//active campaign api key
-define('AC_API_KEY', '5bfc43c86c8e38583c8338874e36d818add8252bea8ce33a387f66f161f762f27a50ff3c');
-define('AC_EVENT_KEY', 'f08433103bedd01a0edef09ae8f12d9886db9f89');
