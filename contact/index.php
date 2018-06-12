8d3c01e59c009a50b571c3fd104c4da12d8ebb6a
Alejandro Sanchez <aalejo@gmail.com>
2018-06-12 15:23:18 +0000

    contacts api

--- contact/index.php
+++ contact/index.php
@@ -1,6 +1,6 @@
 <?php
-	require_once('../../vendor/autoload.php');
-	require_once('../../globals.php');
+	require_once('../vendor/autoload.php');
+	require_once('../globals.php');
 	require_once('../SlimAPI.php');
 	require('routes.php');
 	
